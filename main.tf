93bfd143ccb5bf1ba648737f4f0922851d62c4f6
iftikhar-dot <59255661+iftikhar-dot@users.noreply.github.com>
2021-04-03 13:32:59 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
-provider "aws" {
-  region = var.region
-  profile = var.profile
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
-# get caller identity
-data "aws_caller_identity" "current" {}
+# Configure the AWS Provider
+provider "aws" {
+  region                  = "us-west-2"
+  shared_credentials_file = "/Users/tf_user/.aws/creds"
+  profile                 = "MyAWS"
+}
