04c506660e16bcfeb66aaea7c57917b9afb114a5
iftikhar-dot <59255661+iftikhar-dot@users.noreply.github.com>
2021-04-03 14:23:55 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region                  = "us-east-1"
+  region                  = "us-west-2"
   shared_credentials_file = "/Users/tf_user/.aws/creds"
   profile                 = "MyAWS"
 }
